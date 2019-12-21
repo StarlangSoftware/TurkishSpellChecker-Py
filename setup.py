5ac4708392f6488b79c705387c238c353a921e82
Olcay Taner YILDIZ <olcaytaner@isikun.edu.tr>
2019-12-21 13:35:29 +0300

    Added package requirements.

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name='NlpToolkit-SpellChecker',
-    version='1.0.0',
+    version='1.0.1',
     packages=['SpellChecker'],
     url='https://github.com/olcaytaner/TurkishSpellChecker-Py',
     license='',
     author='olcaytaner',
     author_email='olcaytaner@isikun.edu.tr',
-    description='Turkish Spell Checker Library'
+    description='Turkish Spell Checker Library',
+    install_requires=['NlpToolkit-MorphologicalAnalysis', 'NlpToolkit-Corpus', 'NlpToolkit-NGram']
 )
