c527f9b12042b4c73bbfd3480d49a80a2f5e023d
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-01-02 13:12:09 +0300

    Updated e-mail and url.

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='NlpToolkit-SpellChecker',
     version='1.0.18',
     packages=['SpellChecker'],
-    url='https://github.com/olcaytaner/TurkishSpellChecker-Py',
+    url='https://github.com/StarlangSoftware/TurkishSpellChecker-Py',
     license='',
     author='olcaytaner',
-    author_email='olcaytaner@isikun.edu.tr',
+    author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Turkish Spell Checker Library',
     install_requires=['NlpToolkit-MorphologicalAnalysis', 'NlpToolkit-NGram']
 )
