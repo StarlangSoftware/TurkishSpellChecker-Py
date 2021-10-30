c590c4e20c813ec713483aaeea29875428e56149
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 19:56:49 +0300

    Updated setup.py, dictionary.

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='NlpToolkit-SpellChecker',
-    version='1.0.18',
+    version='1.0.19',
     packages=['SpellChecker'],
     url='https://github.com/StarlangSoftware/TurkishSpellChecker-Py',
     license='',
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Turkish Spell Checker Library',
-    install_requires=['NlpToolkit-MorphologicalAnalysis', 'NlpToolkit-NGram']
+    install_requires=['NlpToolkit-MorphologicalAnalysis', 'NlpToolkit-NGram'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
