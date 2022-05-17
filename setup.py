cd91c6d1c6a743d96b13548d0bceac7b5aa431cb
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-05-17 23:31:06 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='NlpToolkit-SpellChecker',
-    version='1.0.19',
+    version='1.0.20',
     packages=['SpellChecker'],
     url='https://github.com/StarlangSoftware/TurkishSpellChecker-Py',
     license='',
