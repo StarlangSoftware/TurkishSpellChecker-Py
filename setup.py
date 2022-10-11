7905ff6ef70b4334d59eddf014b98918a5518021
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-10-11 15:38:16 +0300

    Moved previousPreviousWord and nextNextWord control one level down.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='NlpToolkit-SpellChecker',
-    version='1.0.22',
+    version='1.0.23',
     packages=['SpellChecker'],
     package_data={'SpellChecker.data': ['*.txt']},
     url='https://github.com/StarlangSoftware/TurkishSpellChecker-Py',
